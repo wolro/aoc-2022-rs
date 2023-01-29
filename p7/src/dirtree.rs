689cee83b502b0f660ce396241b9e542358645cf
Wolfgang Rohringer <w.rohringer@xarion.com>
2023-01-29 21:59:20 +0100

    Add working solution for day 7.

--- p7/src/dirtree.rs
+++ p7/src/dirtree.rs
@@ -1,16 +1,29 @@
 /// Arena tree node representing a directory. Inspiration:
 /// https://dev.to/deciduously/no-more-tears-no-more-knots-arena-allocated-trees-in-rust-44k6
+///
+/// Beware: The original tree from the article had a "node" function used to
+/// create a new node or return its index depending on whether a node with the
+/// provided name is already present in the tree.
+/// This was not compatible with the example, however, since the names of the directories
+/// (=nodes) are not unique! This caused wrong (even circular) parent-child relationships,
+/// and an infinite recursion in the "add_subdir_sizes" function from the main file,
+/// causing a stack overflow :-) The adapted version below works.
+
+/// Tree node. Each node has an index which is used to identify it. Consequently, also parent
+/// and child nodes are represented by their indices, stored in the "parent" Option or
+/// the "children" vector - in contrast to "regular" trees, where parents and children are linked
+/// by references, making up a linked list - which is apparently tricky to implement without
+/// invoking the wrath of the borrow checker.
 #[derive(Debug)]
-pub struct Node
-{
+pub struct Node {
     pub idx: usize,
     pub name: String,
     pub size: u32,
     pub parent: Option<usize>,
     pub children: Vec<usize>,
 }
-impl Node
-{
+impl Node {
+    /// Constructor.
     fn new(idx: usize, name: String, val: u32) -> Self {
         Self {
             idx,
@@ -20,28 +33,22 @@ impl Node
             children: vec![],
         }
     }
-
+    /// This function is used to update the size corresponding to a directory node when
+    /// encountering a file belonging to the directory in the input data.
     pub fn add_filesize(&mut self, filesize: u32) {
         self.size += filesize;
     }
 }
 
-/// Arena tree representing directory tree.
+/// "Arena" representing the directory tree. Basically just a flat vector with nodes, each
+/// of them identified by its index.
 #[derive(Debug, Default)]
-pub struct DirTree
-{
+pub struct DirTree {
     pub arena: Vec<Node>,
 }
-impl DirTree
-{
-    pub fn node(&mut self, name: String, val: u32) -> usize {
-        //first see if it exists
-        for node in &self.arena {
-            if node.name == name {
-                return node.idx;
-            }
-        }
-        // Otherwise, add new node
+impl DirTree {
+    /// Creates a new node and pushes it into the arena vector.
+    pub fn create_node(&mut self, name: String, val: u32) -> usize {
         let idx = self.arena.len();
         self.arena.push(Node::new(idx, name, val));
         idx
--- p7/src/main.rs
+++ p7/src/main.rs
@@ -1,64 +1,139 @@
 /// Advent of Code day 7
 /// https://adventofcode.com/2022/day/7
 ///
-/// Oh, we have to deal with a tree. Now I know trees in Rust can be painful.
-/// Digging around, it seems that "Arena-Allocated Trees" are the way to go.
-/// There are a bunch of crates, but to me it seems reasonable to just use
+/// Oh, we could resolve this using a tree. Now I know trees in Rust can be painful.
+/// Digging around, it seems that "Arena-allocated Trees" are the way to go.
+/// There are a bunch of crates, but to me it would seem reasonable to just use
 /// "petgraph" (supporting all kinds of graphs, API looks nice)
 ///
-/// Since we are here to learn, we will try to roll our own! Taking this
-/// article as inspiration:
+/// Since we are here to learn, we will try to roll our own (see module "dirtree")!
+/// Taking this article as inspiration:
 /// https://dev.to/deciduously/no-more-tears-no-more-knots-arena-allocated-trees-in-rust-44k6
-
 mod dirtree;
-use dirtree::DirTree;
 use anyhow::Result;
+use dirtree::DirTree;
 
+const SPACE_TOTAL: u32 = 70000000;
+const SPACE_UPDATE: u32 = 30000000;
 
-fn build_dirtree(lines: Vec<&str>) -> Result<DirTree>{
+/// Parse the input data and fill up the tree accordingly.
+fn build_dirtree(lines: Vec<&str>) -> Result<DirTree> {
     // initialize tree
     let mut dir_tree: DirTree = DirTree::default();
-    let root_idx  = dir_tree.node("/".to_owned(), 0);
+    let root_idx = dir_tree.create_node("/".to_owned(), 0);
     let mut currdir_idx = root_idx;
 
     // build directory tree from input
     for log_line in lines {
         let split_line = log_line.split(' ').collect::<Vec<&str>>();
-        match split_line [..]{
-            ["$", "ls"] => {},
-            ["dir", _] => {},
-            ["$", "cd", _ ] => {
-                match split_line[2] {
-                    "/" => {},
-                    ".." => {
-                        currdir_idx = dir_tree.arena[currdir_idx].parent.unwrap();
-                    },
-                    _ => {
-                            let node_idx  = dir_tree.node(split_line[2].to_owned(), 0);
-                            dir_tree.arena[currdir_idx].children.push(node_idx);
-                            dir_tree.arena[node_idx].parent = Some(currdir_idx);
-                            currdir_idx = node_idx;
-                        },
-                    }
-
-                },
-            _ => { dir_tree.arena[currdir_idx].add_filesize(split_line[0].parse::<u32>().unwrap()) },
+        match split_line[..] {
+            // matching to slices is nifty
+            ["$", "ls"] => {} // we can ignore the "ls" command
+            ["dir", _] => {}  // we can ignore directory entries until we cd there
+            ["$", "cd", _] => match split_line[2] {
+                "/" => {} // we already took care of the root folder
+                ".." => {
+                    currdir_idx = dir_tree.arena[currdir_idx].parent.unwrap();
+                } // go back to parent directory
+                _ => {
+                    let node_idx = dir_tree.create_node(split_line[2].to_owned(), 0);
+                    dir_tree.arena[currdir_idx].children.push(node_idx);
+                    dir_tree.arena[node_idx].parent = Some(currdir_idx);
+                    currdir_idx = node_idx;
+                } // We update our tree when changing to a new directory. Currently,
+                           // changing to directories already present in our tree is not handled
+                           // properly here and would cause issues; luckily, this doesn't seem
+                           // to happen in this example.
+            },
+            _ => dir_tree.arena[currdir_idx].add_filesize(split_line[0].parse::<u32>().unwrap()),
         }
     }
 
     Ok(dir_tree)
 }
 
-
 fn main() -> Result<()> {
-    // read data
-    let lines = include_str!("../test_input.txt")
-        .lines()
-        .collect::<Vec<_>>();
-
-    let dir_tree = build_dirtree(lines);
-    dbg!(&dir_tree);
+    // inline data
+    let lines = include_str!("../input.txt").lines().collect::<Vec<_>>();
+    // build directory tree
+    let dir_tree = build_dirtree(lines)?;
+    // solution to part 1
+    let sum_lt100k = get_sum_lt100k(&dir_tree)?;
+    println!("Sum of all directories of size <100 kb: {}", sum_lt100k);
+    // solution to part 2
+    let suitable_dir_size = find_dir_to_del(&dir_tree)?;
+    println!(
+        "Size of the smallest directory we can delete so that the update fits on our disk: {}",
+        suitable_dir_size
+    );
 
     Ok(())
 }
 
+/// Solution to part 1: find the total size of all directories, each with sizes
+/// smaller than 100 kb.
+fn get_sum_lt100k(dir_tree: &DirTree) -> Result<u32> {
+    let mut sum_lt100k: u32 = 0;
+    for dir in &dir_tree.arena {
+        let mut total_size = dir.size;
+        add_subdir_sizes(dir, &mut total_size, dir_tree);
+        if total_size < 100000 {
+            sum_lt100k += total_size;
+        }
+    }
+    Ok(sum_lt100k)
+}
+
+/// Solution to part 2: find the size of the smallest folder that we can delete
+/// so that the update fits on the disk.
+fn find_dir_to_del(dir_tree: &DirTree) -> Result<u32> {
+    let mut used_space = dir_tree.arena[0].size;
+    add_subdir_sizes(&dir_tree.arena[0], &mut used_space, dir_tree);
+
+    let mut suitable_dir_sizes: Vec<u32> = Vec::new();
+    for dir in &dir_tree.arena {
+        let mut total_size = dir.size;
+        add_subdir_sizes(dir, &mut total_size, dir_tree);
+        if total_size > SPACE_UPDATE - (SPACE_TOTAL - used_space) {
+            suitable_dir_sizes.push(total_size)
+        }
+    }
+    Ok(*suitable_dir_sizes
+        .iter()
+        .min()
+        .expect("Couldn't find suitable directory to delete."))
+}
+
+/// Function that recursively adds the size of subdirectories to the size of all files
+/// in the current directory, to get the cumulative directory size.
+fn add_subdir_sizes(dir: &dirtree::Node, total_size: &mut u32, dir_tree: &DirTree) {
+    let subdirs = dir.children.clone();
+    for subdir in subdirs {
+        *total_size += dir_tree.arena[subdir].size;
+        add_subdir_sizes(&dir_tree.arena[subdir], total_size, dir_tree);
+    }
+}
+
+/// Check if algorithm for part 1 works on test input (see puzzle description).
+#[test]
+fn test_sum_lt100k_exampledata_part1() {
+    // inline test data
+    let lines = include_str!("../test_input.txt")
+        .lines()
+        .collect::<Vec<_>>();
+
+    let dir_tree = build_dirtree(lines).unwrap();
+    assert_eq!(get_sum_lt100k(&dir_tree).unwrap(), 95437);
+}
+
+/// Check if algorithm for part 2 works on test input (see puzzle description).
+#[test]
+fn test_find_dir_to_del_part2() {
+    // inline test data
+    let lines = include_str!("../test_input.txt")
+        .lines()
+        .collect::<Vec<_>>();
+
+    let dir_tree = build_dirtree(lines).unwrap();
+    assert_eq!(find_dir_to_del(&dir_tree).unwrap(), 24933642);
+}
