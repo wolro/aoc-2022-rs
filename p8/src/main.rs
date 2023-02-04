b1d09308c73c218630aa705db14e657e0b9ef92e
Wolfgang Rohringer <w.rohringer@xarion.com>
2023-02-05 00:37:05 +0100

    formatting

--- p8/src/main.rs
+++ p8/src/main.rs
@@ -1,11 +1,10 @@
 /// Advent of Code day 8
 /// https://adventofcode.com/2022/day/8
-/// 
+///
 /// Helping the elves with their tree house was straightforward enough.
 /// This solution is probably not very elegant (one could probably
-/// define a proper grid type with its own iterator to traverse it), 
+/// define a proper grid type with its own iterator to traverse it),
 /// but it worked without having to debug a whole lot.
-
 use anyhow::Result;
 
 /// parse forest into two vectors represending grid lines and columns, respectively.
@@ -36,33 +35,28 @@ fn get_grid_vecs(lines: Vec<&str>) -> Result<(Vec<Vec<u32>>, Vec<Vec<u32>>)> {
 fn is_visible(lines: &Vec<Vec<u32>>, coord_1: usize, coord_2: usize) -> Result<bool> {
     let reduced_line = lines[coord_1].clone();
     let tree_size = reduced_line[coord_2];
-    
-    let v_one = reduced_line[..coord_2]
+
+    let v_one = reduced_line[..coord_2].iter().any(|&idx| idx >= tree_size);
+    let v_two = reduced_line[coord_2 + 1..]
         .iter()
         .any(|&idx| idx >= tree_size);
-    let v_two = reduced_line[coord_2+1..]
-        .iter()
-        .any(|&idx| idx >= tree_size);
-    
+
     Ok(!v_one || !v_two)
 }
 
 /// Calculate number of visible trees.
 fn nr_visible_trees(int_cols: &Vec<Vec<u32>>, int_lines: &Vec<Vec<u32>>) -> Result<u32> {
     let mut vis_cnt: u32 = 0;
-    for tree_x in 1..&int_cols.len()-1 {
-        for tree_y in 1..&int_lines.len()-1 {
+    for tree_x in 1..&int_cols.len() - 1 {
+        for tree_y in 1..&int_lines.len() - 1 {
             if is_visible(int_lines, tree_x, tree_y)? || is_visible(int_cols, tree_y, tree_x)? {
                 vis_cnt += 1;
             }
         }
     }
-    let grid_shape = (
-        int_lines.len() as u32,
-        int_cols.len() as u32,
-    );
+    let grid_shape = (int_lines.len() as u32, int_cols.len() as u32);
 
-    vis_cnt = vis_cnt + 2*grid_shape.0 + 2*grid_shape.1 - 4;
+    vis_cnt = vis_cnt + 2 * grid_shape.0 + 2 * grid_shape.1 - 4;
     Ok(vis_cnt)
 }
 
@@ -72,12 +66,12 @@ fn nr_visible_trees(int_cols: &Vec<Vec<u32>>, int_lines: &Vec<Vec<u32>>) -> Resu
 fn viewblockers(lines: &Vec<Vec<u32>>, coord_1: usize, coord_2: usize) -> (usize, usize) {
     let reduced_line = lines[coord_1].clone();
     let tree_size = reduced_line[coord_2];
-    
+
     let v_one = reduced_line[..coord_2]
         .iter()
         .rev()
         .position(|&idx| idx >= tree_size);
-    let v_two = reduced_line[coord_2+1..]
+    let v_two = reduced_line[coord_2 + 1..]
         .iter()
         .position(|&idx| idx >= tree_size);
 
@@ -93,34 +87,37 @@ fn viewblockers(lines: &Vec<Vec<u32>>, coord_1: usize, coord_2: usize) -> (usize
     (one, two)
 }
 
-
 /// Calculate scenic score from distances determined by the "viewblockers()" function.
-fn get_scenic_score(int_cols: &Vec<Vec<u32>>, int_lines: &Vec<Vec<u32>>, coord_1: usize, coord_2: usize) -> Result<u32> {
+fn get_scenic_score(
+    int_cols: &Vec<Vec<u32>>,
+    int_lines: &Vec<Vec<u32>>,
+    coord_1: usize,
+    coord_2: usize,
+) -> Result<u32> {
     // vertical grid direction
     let (a, b) = viewblockers(&int_cols, coord_1, coord_2);
     // horizontal grid direction
     let (c, d) = viewblockers(&int_lines, coord_2, coord_1);
 
-    Ok((a*b*c*d).try_into().unwrap())
+    Ok((a * b * c * d).try_into().unwrap())
 }
 
-
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let lines = include_str!("../input.txt").lines().collect::<Vec<_>>();
     let (int_lines, int_cols) = get_grid_vecs(lines)?;
 
     // solution for part 1
     let vis_cnt = nr_visible_trees(&int_cols, &int_lines)?;
-    println!("The number of visible trees is: {}", vis_cnt);    
+    println!("The number of visible trees is: {}", vis_cnt);
 
     // solution for part 2
     let mut max_scenic_score: u32 = 0;
-    for tree_x in 1..&int_cols.len()-1 {
-        for tree_y in 1..&int_lines.len()-1 {
+    for tree_x in 1..&int_cols.len() - 1 {
+        for tree_y in 1..&int_lines.len() - 1 {
             let current_score = get_scenic_score(&int_lines, &int_cols, tree_x, tree_y)?;
             if current_score > max_scenic_score {
                 max_scenic_score = current_score;
-            }            
+            }
         }
     }
     println!("The best tree has a scenic score of: {}", max_scenic_score);
@@ -131,7 +128,9 @@ fn main() -> Result<()>{
 /// Test for part 1.
 #[test]
 fn check_find_visible_trees_on_testdata() {
-    let lines = include_str!("../input_test.txt").lines().collect::<Vec<_>>();
+    let lines = include_str!("../input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
     let (int_lines, int_cols) = get_grid_vecs(lines).unwrap();
     assert_eq!(nr_visible_trees(&int_cols, &int_lines).unwrap(), 21)
 }
@@ -139,14 +138,16 @@ fn check_find_visible_trees_on_testdata() {
 /// Test for part 2.
 #[test]
 fn check_find_viewblockers_on_testdata() {
-    let lines = include_str!("../input_test.txt").lines().collect::<Vec<_>>();
+    let lines = include_str!("../input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
     let (int_lines, int_cols) = get_grid_vecs(lines).unwrap();
 
     let (a, b) = viewblockers(&int_cols, 2, 1);
     let (c, d) = viewblockers(&int_lines, 1, 2);
-    assert_eq!(a*b*c*d, 4);
+    assert_eq!(a * b * c * d, 4);
 
     let (a, b) = viewblockers(&int_cols, 2, 3);
     let (c, d) = viewblockers(&int_lines, 3, 2);
-    assert_eq!(a*b*c*d, 8);
+    assert_eq!(a * b * c * d, 8);
 }
