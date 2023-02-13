f4c9e3d793b45730d25e2302814c08f0f7724baa
Wolfgang Rohringer <w.rohringer@xarion.com>
2023-02-13 23:54:00 +0100

    Solution for day 11.

--- p11/src/main.rs
+++ p11/src/main.rs
@@ -1,23 +1,39 @@
-use std::{thread::current, collections::VecDeque, iter::Enumerate};
-
-/// Advent of Code day 10
-/// https://adventofcode.com/2022/day/10
+/// Advent of Code day 11
+/// https://adventofcode.com/2022/day/11
 ///
 /// Monkeying around with a hand-written parser, again. At some point I will look into this "nom" crate. But not today.
+///
+/// Otherwise, needed to look at other people's solutions for hints on this.
+/// First, issues with borrow checker with loops accessing both the monkey vector
+/// and its elements. Resolved by looping over index and having only a mutable borrow
+/// to a singular monkey out of the vector.
+///
+/// Second, I needed hints for part 2. Never heard of the "Chinese Remainder Theorem"
+/// before. On the bright side, I made use of passing functions as arguments for the
+/// first time in Rust.
 
+/// The monkey struct.
 #[derive(Debug, Clone, Default)]
 struct Monkey {
-    items: VecDeque<u64>,
-    div: u64,
-    op_op: char,
-    fac: Option<u64>,
-    receiver_true: u64,
-    receiver_false: u64,
-    insp_items: u64,
+    items: Vec<u64>,
+    div: u64,            // Divisor, see input data
+    op_op: char,         // Operator used in "Operation" (+ or *), see input data
+    fac: Option<u64>, // Second operand for "Operation". If None, use "item" (worry value), see e.g. Monkey 2 in test input data.
+    receiver_true: u64, // Index of monkey being thrown to if test condition is true
+    receiver_false: u64, // Index of monkey being thrown to if test condition is false
+    insp_items: u64,  // Number of times this monkey has inspected an item
 }
 impl Monkey {
-    fn insp_op(item: &u64, op: &char, fac: &Option<u64>) -> u64 {
-        let mut operand: u64;
+    /// Model inspection operation. Takes function "f" to apply "worry reduction"
+    /// (either division or remainder for part 1 and 2, respectively).
+    fn insp_op(
+        item: &u64,
+        op: &char,
+        fac: &Option<u64>,
+        f: &dyn Fn(u64, u64) -> u64,
+        worry_div: u64,
+    ) -> u64 {
+        let operand: u64;
         if let Some(op) = fac {
             operand = *op;
         } else {
@@ -25,8 +41,8 @@ impl Monkey {
         }
 
         match op {
-            '+' => (*item + operand)/3,
-            '*' => (*item * operand)/3,
+            '+' => f(*item + operand, worry_div),
+            '*' => f(*item * operand, worry_div),
             _ => {
                 println!("Can't update item value, error with item or Monkey?");
                 *item
@@ -34,6 +50,7 @@ impl Monkey {
         }
     }
 
+    /// Returns monkey an "item" is thrown to.
     fn receiver(&self, item: &u64) -> usize {
         if *item % self.div == 0 {
             self.receiver_true.try_into().unwrap()
@@ -43,6 +60,19 @@ impl Monkey {
     }
 }
 
+/// Divide worry by 3 after inspection, see instructions for part 1.
+fn worry_reducer_part1(worry: u64, reducer: u64) -> u64 {
+    worry / reducer
+}
+
+/// Remainder operation to reduce ginormous "worry levels".
+fn worry_reducer_part2(value: u64, reducer: u64) -> u64 {
+    value % reducer
+}
+
+/// The great monkey parser. Didn't want to rely on fixed string splitting positions,
+/// so I matched on the contents of the "prefix" (part before colon). Made the parser
+/// more verbose, but so be it.
 fn monkey_parser(input_data: Vec<&str>) -> Vec<Monkey> {
     // split input into segments corresponding to each monkey
     let input_monkeys: Vec<_> = input_data
@@ -72,9 +102,9 @@ fn monkey_parser(input_data: Vec<&str>) -> Vec<Monkey> {
                         current_monkey.items = split_line[1]
                             .split(',')
                             .map(|e| e.trim().parse::<u64>().expect("Error parsing items."))
-                            .collect::<VecDeque<u64>>();
-                    } else if let Ok(sole_item) = split_line[1].parse::<u64>() {
-                        current_monkey.items.push_back(sole_item);
+                            .collect::<Vec<u64>>();
+                    } else if let Ok(sole_item) = split_line[1].trim().parse::<u64>() {
+                        current_monkey.items.push(sole_item);
                     }
                 }
                 // parse operation
@@ -120,7 +150,8 @@ fn monkey_parser(input_data: Vec<&str>) -> Vec<Monkey> {
     monkeys
 }
 
-// helper function for "monkey_parser" to extract struct field values from input data
+/// Helper function for "monkey_parser" to extract struct field values
+/// for "div", "receiver_true" and "receiver_false" from input data.
 fn parse_splitline(split_line: &[String], field: &str) -> u64 {
     let err_str = format!("Error parsing {field}.");
     split_line[1]
@@ -131,34 +162,61 @@ fn parse_splitline(split_line: &[String], field: &str) -> u64 {
         .unwrap()
 }
 
-fn main() {
-    let input_data = include_str!("../input_test.txt")
-        .lines()
-        .collect::<Vec<_>>();
-    let mut monkeys = monkey_parser(input_data);
-    for _round in 0..1 {
-        for mut monkey in &mut monkeys {
-            monkey.items = monkey.items.iter()
-                .map(|e| {
-                    monkey.insp_items += 1;
-                    Monkey::insp_op(e, &monkey.op_op, &monkey.fac) })
-                .collect();
+/// Execute one round of inspection and throwing.
+fn execute_round(monkeys: &mut Vec<Monkey>, reducer: &dyn Fn(u64, u64) -> u64, worry_div: u64) {
+    for m_idx in 0..monkeys.len() {
+        let monkey = &mut monkeys[m_idx];
+        // inspect and update worry levels
+        monkey.items = monkey
+            .items
+            .iter_mut()
+            .map(|e| {
+                monkey.insp_items += 1;
+                Monkey::insp_op(e, &monkey.op_op, &monkey.fac, reducer, worry_div)
+            })
+            .collect();
+        // throw items
+        for (i_idx, item) in monkeys[m_idx].clone().items.iter().enumerate() {
+            let rec_idx = monkeys[m_idx].receiver(&monkeys[m_idx].items[i_idx]);
+            monkeys[rec_idx].items.push(*item);
         }
-        let nr_monkeys = monkeys.len();
-        for m_idx in 0..nr_monkeys {
-            for(i_idx, item) in monkeys[m_idx].clone().items.iter().enumerate() {
-                let rec_idx = monkeys[m_idx].receiver(&monkeys[m_idx].items[i_idx]);
-                monkeys[rec_idx].items.push_back(*item);
-            }
-            monkeys[m_idx].items.clear();
-        }
-
-
+        monkeys[m_idx].items.clear();
     }
-    dbg!(&monkeys);
-    let mut inspections = monkeys.iter()
-    .map(|e| e.insp_items )
-    .collect::<Vec<_>>();
+}
+
+fn main() {
+    solution_part1();
+    solution_part2();
+}
+
+fn solution_part1() {
+    let input_data = include_str!("../input.txt").lines().collect::<Vec<_>>();
+    let mut monkeys = monkey_parser(input_data);
+
+    for _round in 0..20 {
+        execute_round(&mut monkeys, &worry_reducer_part1, 3);
+    }
+    // get number of inspections performed by each monkey
+    let mut inspections = monkeys.iter().map(|e| e.insp_items).collect::<Vec<_>>();
+
+    inspections.sort(); // sort in ascending order
+    let inspections = inspections.iter().rev().collect::<Vec<_>>(); // reverse order
+    let score = inspections[0] * inspections[1]; // score, see puzzle description
+    dbg!(score);
+}
+
+fn solution_part2() {
+    let input_data = include_str!("../input.txt").lines().collect::<Vec<_>>();
+    let mut monkeys = monkey_parser(input_data);
+    let div_prod = monkeys.iter().map(|e| e.div).product();
+    // TBH i have no idea why the remainder operation using "div_prod"
+    // leaves the division check unaffected. But I am not sure if I care sufficiently.
+
+    for _round in 0..10000 {
+        execute_round(&mut monkeys, &worry_reducer_part2, div_prod);
+    }
+
+    let mut inspections = monkeys.iter().map(|e| e.insp_items).collect::<Vec<_>>();
 
     inspections.sort();
     let inspections = inspections.iter().rev().collect::<Vec<_>>();
@@ -166,3 +224,41 @@ fn main() {
     dbg!(score);
 }
 
+#[test]
+fn test_monkey_slinging_part1() {
+    let input_data = include_str!("../input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
+    let mut monkeys = monkey_parser(input_data);
+
+    for _round in 0..20 {
+        execute_round(&mut monkeys, &worry_reducer_part1, 3);
+    }
+
+    let mut inspections = monkeys.iter().map(|e| e.insp_items).collect::<Vec<_>>();
+
+    inspections.sort();
+    let inspections = inspections.iter().rev().collect::<Vec<_>>();
+    let score = inspections[0] * inspections[1];
+    assert_eq!(score, 10605);
+}
+
+#[test]
+fn test_monkey_slinging_part2() {
+    let input_data = include_str!("../input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
+    let mut monkeys = monkey_parser(input_data);
+    let div_prod = monkeys.iter().map(|e| e.div).product();
+
+    for _round in 0..10000 {
+        execute_round(&mut monkeys, &worry_reducer_part2, div_prod);
+    }
+
+    let mut inspections = monkeys.iter().map(|e| e.insp_items).collect::<Vec<_>>();
+
+    inspections.sort();
+    let inspections = inspections.iter().rev().collect::<Vec<_>>();
+    let score = inspections[0] * inspections[1];
+    assert_eq!(score, 2713310158);
+}
