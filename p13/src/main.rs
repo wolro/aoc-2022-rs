421a713fe4b3ea266c79e0475efc5e29d2b23d6c
Wolfgang Rohringer <w.rohringer@xarion.com>
2023-04-23 01:59:16 +0200

    Added comp_packets function which hofefully will work :-)

--- p13/src/main.rs
+++ p13/src/main.rs
@@ -2,35 +2,46 @@
 /// https://adventofcode.com/2022/day/13
 use anyhow::Result;
 use serde_json::Value;
+use std::cmp::{max, Ordering};
 
 fn main() -> Result<()> {
     let input_data = include_str!("../input_test.txt")
         .lines()
         .collect::<Vec<_>>();
-    // dbg!(input_data);
 
-    let l_packet_str = input_data[7];
+    let l_packet_str = input_data[3];
     let r_packet_str = input_data[4];
 
-    let l_packet_json: Value = serde_json::from_str(l_packet_str)?;
-    let l_packet_json: Value = serde_json::from_str(l_packet_str)?;
+    let l_packet: Value = serde_json::from_str(l_packet_str)?;
+    let r_packet: Value = serde_json::from_str(r_packet_str)?;
 
-    dbg!(l_packet_json.is_array());
-
-    // let l_packet_array = get_array(l_packet_str)?;
-    dbg!(&l_packet_json);
+    let l_vs_r: Ordering = comp_packets(&l_packet, &r_packet);
 
     Ok(())
 }
 
-fn get_array(l_packet_str: &str) -> Result<Vec<i64>> {
-    let l_packet_json: Value = serde_json::from_str(l_packet_str)?;
-    let l_packet_array: Vec<i64> = l_packet_json
-        .as_array()
-        .unwrap()
-        .iter()
-        .map(|e| e.as_i64().unwrap())
-        .collect();
-
-    Ok(l_packet_array)
+fn comp_packets(l_packet: &Value, r_packet: &Value) -> Ordering {
+    match (l_packet, r_packet) {
+        (Value::Number(x), Value::Number(y)) => x.as_i64().unwrap().cmp(&y.as_i64().unwrap()),
+        (Value::Array(a), Value::Array(b)) => {
+            for i in 0..max(a.len(), b.len()) {
+                match (a.get(i), b.get(i)) {
+                    (None, _) => return Ordering::Less,
+                    (_, None) => return Ordering::Greater,
+                    (Some(x), Some(y)) => match comp_packets(x, y) {
+                        Ordering::Equal => {}
+                        c => return c,
+                    },
+                }
+            }
+            Ordering::Equal
+        }
+        (Value::Array(_), Value::Number(_)) => {
+            comp_packets(l_packet, &Value::Array(vec![r_packet.clone()]))
+        }
+        (Value::Number(_), Value::Array(_)) => {
+            comp_packets(&Value::Array(vec![l_packet.clone()]), r_packet)
+        }
+        _ => unreachable!(),
+    }
 }
