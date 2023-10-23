f5b8782671c671155a74885a9bcca638a606cb94
Wolfgang Rohringer <w.rohringer@xarion.com>
2023-10-24 01:11:59 +0200

    Scaffolding for p15.

---
+++ p15/src/main.rs
@@ -0,0 +1,20 @@
+/// Advent of Code day 15
+/// https://adventofcode.com/2022/day/15
+
+use anyhow::Result;
+
+fn main() {
+    let lines = include_str!("../input_test.txt").lines().collect::<Vec<_>>();    
+}
+
+
+
+#[test]
+fn part1_validate_on_testdata() {
+    unimplemented!();
+}
+
+#[test]
+fn part2_validate_on_testdata() {
+    unimplemented!();
+}
